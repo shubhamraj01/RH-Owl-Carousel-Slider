c1f1ab1b44918c91172bbad1836fee0ae489335d
RHapani <rohanhhapani@gmail.com>
2021-04-10 13:19:39 +0530

    Owl carousel slider in Magento 2

---
+++ view/frontend/requirejs-config.js
@@ -0,0 +1,13 @@
+/**
+ * Created By : Rohan Hapani
+ */
+var config = {
+    paths: {
+        owlcarousel: "RH_OwlSlider/js/owl.carousel"
+    },
+    shim: {
+        owlcarousel: {
+            deps: ['jquery']
+        }
+    }
+};
